60895025f4032d98d35a5adf77b3096fe8d9151a
Thisara Rathnapala <thisaratr94@gmail.com>
2023-12-15 18:48:14 +0530

    Implement back-end database connection

---
+++ back-end/src/main/java/lk/ijse/dep11/app/WebRootConfig.java
@@ -0,0 +1,2 @@
+package lk.ijse.dep11.app;public class WebRootConfig {
+}
---
+++ back-end/src/main/java/lk/ijse/dep11/app/api/TaskHttpController.java
@@ -0,0 +1,26 @@
+package lk.ijse.dep11.app.api;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/tasks")
+@CrossOrigin
+public class HttpController {
+
+    private final HikariDataSource pool;
+
+    public HttpController(Environment env) {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(env.getRequiredProperty("spring.datasource.url"));
+        config.setUsername(env.getRequiredProperty("spring.datasource.username"));
+        config.setPassword(env.getRequiredProperty("spring.datasource.password"));
+        config.setDriverClassName(env.getRequiredProperty("spring.datasource.driver-class-name"));
+        config.setMaximumPoolSize(env.getRequiredProperty("spring.datasource.hikari.maximum-pool-size", Integer.class));
+        pool = new HikariDataSource(config);
+    }
+}
