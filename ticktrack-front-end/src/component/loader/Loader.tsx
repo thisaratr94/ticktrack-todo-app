996c27df15fc84f267f53a98fb46ff7ee92a4d4d
Thisara Rathnapala <thisaratr94@gmail.com>
2023-12-16 23:02:22 +0530

    Implement loader component

--- ticktrack-front-end/src/component/loader/Loader.tsx
+++ ticktrack-front-end/src/component/loader/Loader.tsx
@@ -1,5 +1,19 @@
+import './Loader.css'
+
 export function Loader() {
     return (
-        <></>
+        <div className="Loader d-flex vh-100
+                justify-content-center align-items-center">
+            <div className="lds-roller">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
     );
 }
