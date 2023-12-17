de5fb70797f4ece161aa88d959b97198a221572d
Thisara Rathnapala <thisaratr94@gmail.com>
2023-12-17 20:27:17 +0530

    Update index.html

--- ticktrack-front-end/src/App.tsx
+++ ticktrack-front-end/src/App.tsx
@@ -1,10 +1,49 @@
 import './App.css'
+import {useEffect, useState} from "react";
+import {onAuthStateChanged} from 'firebase/auth';
+import {auth} from "./firebase.ts";
+import {useUser, useUserDispatcher} from "./context/UserContext.tsx";
+import {SignIn} from "./component/signin/SignIn.tsx";
+import {Loader} from "./component/loader/Loader.tsx";
+import {Header} from "./component/header/Header.tsx";
+import {Form} from "./component/form/Form.tsx";
+import {TaskProvider, useTaskDispatcher} from "./context/TaskContext.tsx";
+import {TaskList} from "./component/task-list/TaskList.tsx";
 
 function App() {
 
+  const [loader, setLoader] = useState(true);
+  const user = useUser();
+  const userDispatcher = useUserDispatcher();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setLoader(false);
+      if (user) {
+        userDispatcher({type: 'sign-in', user});
+      } else {
+        userDispatcher({type: 'sign-out'});
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
-
+      {loader ?
+          <Loader/>
+          :
+          user?
+              (<>
+                    <Header/>
+                    <TaskProvider>
+                      <Form/>
+                      <TaskList/>
+                    </TaskProvider>
+              </>)
+              :
+              <SignIn/>
+      }
     </>
   )
 }
